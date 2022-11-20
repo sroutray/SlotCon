a020ce25ce192d2ce62e58c1dddf7a341e77e3aa
Sandeep Routray <sandeeproutray1999@gmail.com>
2022-11-20 12:47:48 -0500

    Initialize copy of original files for change

---
+++ scripts/slotcon_coco_r50_800ep_no_predictor_no_center.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export PATH=/h/sroutray/.conda/envs/dfl-3/bin:$PATH
+
+set -e
+set -x
+
+data_dir="/scratch/gobi3/chaenayo/coco/images"
+output_dir="./output/slotcon_coco_baseline_1gpu_800ep_no_predictor"
+
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port 12348 --nproc_per_node=1 \
+    main_pretrain_no_predictor.py \
+    --dataset COCO \
+    --data-dir ${data_dir} \
+    --output-dir ${output_dir} \
+    \
+    --arch resnet50 \
+    --dim-hidden 4096 \
+    --dim-out 256 \
+    --num-prototypes 256 \
+    --teacher-momentum 0.99 \
+    --teacher-temp 0.07 \
+    --group-loss-weight 0.5 \
+    --no-predictor \
+    \
+    --batch-size 80 \
+    --optimizer lars \
+    --base-lr 1.0 \
+    --weight-decay 1e-5 \
+    --warmup-epoch 5 \
+    --epochs 800 \
+    --fp16 \
+    \
+    --print-freq 50 \
+    --save-freq 10 \
+    --auto-resume \
+    --num-workers 8 \
+    --wandb-logging
